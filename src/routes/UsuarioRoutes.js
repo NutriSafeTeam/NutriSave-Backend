7e9171d732a9872a8210dd82e32203c8b67c29c9
RiumeCarlosA <riumecarlos@hotmail.com>
2023-03-19 04:26:07 +0000

    criando autorização

--- src/routes/UsuarioRoutes.js
+++ src/routes/UsuarioRoutes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import UsuarioController from '../controllers/UsuarioController'
 
+import loginRequired from '../middlewares/loginRequired';
+
 const router = new Router();
 
 router.post('/', UsuarioController.create);
-router.get('/', UsuarioController.findAll);
+router.get('/', loginRequired, UsuarioController.findAll);
 router.get('/:id', UsuarioController.findById);
 router.put('/:id', UsuarioController.update);
 router.delete('/:id', UsuarioController.delete);
